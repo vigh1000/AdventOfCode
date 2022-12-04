3ab8c2c11f17bb949cbe98ea72f6df56c46357e8
Virgil Ghizelea <vigh@gmx.de>
2022-12-04 22:31:53 +0100

    Day2 Part2 solved

--- src/main/java/DayTwo.java
+++ src/main/java/DayTwo.java
@@ -10,7 +10,7 @@ public class DayTwo extends DayGeneral {
     HashMap<String,Integer> resultTable = new HashMap<>();
 
     public DayTwo(File inputFile) throws FileNotFoundException {
-        fillResultTable();
+        fillResultTablePartTwo();
         Scanner scanner = new Scanner(inputFile);
         int totalScore = 0;
         while (scanner.hasNextLine()) {
@@ -20,7 +20,7 @@ public class DayTwo extends DayGeneral {
         result = String.valueOf(totalScore);
     }
 
-    void fillResultTable(){
+    void fillResultTablePartOne(){
         resultTable.put("A X", 4);
         resultTable.put("A Y", 8);
         resultTable.put("A Z", 3);
@@ -30,5 +30,17 @@ public class DayTwo extends DayGeneral {
         resultTable.put("C X", 7);
         resultTable.put("C Y", 2);
         resultTable.put("C Z", 6);
+    }
+
+    void fillResultTablePartTwo(){
+        resultTable.put("A X", 3);
+        resultTable.put("A Y", 4);
+        resultTable.put("A Z", 8);
+        resultTable.put("B X", 1);
+        resultTable.put("B Y", 5);
+        resultTable.put("B Z", 9);
+        resultTable.put("C X", 2);
+        resultTable.put("C Y", 6);
+        resultTable.put("C Z", 7);
     }
 }
